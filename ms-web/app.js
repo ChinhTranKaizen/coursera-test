33919daf68c84f50305538968383234d97784796
ChinhTranKaizen <ChinhTranKaizen@users.github.com>
2020-09-19 20:16:15 -0400

    that is enough web-dev for today. I learned the basics!

--- ms-web/app.js
+++ ms-web/app.js
@@ -1,1 +1,20 @@
+'use strict';
 
+console.log('here\'s a hidden message');
+const bodycolor = document.body.style;
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+  if (bodycolor.backgroundColor =="red"){
+    bodycolor.backgroundColor = "white"
+  }
+  else{
+    bodycolor.backgroundColor ="red";
+  }
+
+}
